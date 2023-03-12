2527cb9402bef8683e3323f3f5684366a8de8d0d
acca3434 <wnqudgus1234@gmail.com>
2023-03-12 17:34:33 +0900

    "fix(test) : 20230312 - main fix after build gh-pages"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
+import { Web3Modal,useWeb3Modal } from '@web3modal/react'
 import { configureChains, createClient, WagmiConfig,} from 'wagmi'
 import { arbitrum, mainnet, polygon, bsc, bscTestnet, goerli } from 'wagmi/chains'
 import Homepage from "./view/Homepage"
-
 const chains = [arbitrum, mainnet, polygon, goerli, bsc, bscTestnet];
 const projectId = 'f85ccd6a72bcb04b8fa287ba34b90bf2';
 
@@ -13,14 +13,26 @@ const wagmiClient = createClient({
   connectors: w3mConnectors({ projectId, version: 2, chains }),
   provider
 })
-
 const ethereumClient = new EthereumClient(wagmiClient, chains)
-
 function App() {
+
+const { isOpen, open, close, setDefaultChain } = useWeb3Modal();
+// setDefaultChain("polygon");
+const options = {
+  route: "Account" | "ConnectWallet" | "Help" | "SelectNetwork"
+  };
+  if (isOpen) {
+   open(options);    
+  }
+// Close modal
+  // if (!isOpen) {
+  //   close();
+  // }
   return (
     <>
-      <WagmiConfig client={wagmiClient}/>
-      <Homepage/>
+      <WagmiConfig client={wagmiClient}>
+      <Homepage />정상적으로 바꿔지는지 확인
+      </WagmiConfig>
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     </>
   )
