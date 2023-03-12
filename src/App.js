272e08039ddafceb693d09ebbd8b3b0a9b97a333
acca3434 <wnqudgus1234@gmail.com>
2023-03-12 18:14:19 +0900

    "feat(Web3NetworkSwitch) : 20230312 - add Web3NetworkSwitch"

---
+++ src/App.js
@@ -0,0 +1,65 @@
+
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal,useWeb3Modal,Web3NetworkSwitch} from '@web3modal/react'
+import { configureChains, createClient, WagmiConfig,} from 'wagmi'
+import { arbitrum, mainnet, polygon, bsc, bscTestnet, goerli } from 'wagmi/chains'
+import Homepage from "./view/Homepage"
+const chains = [arbitrum, mainnet, polygon, goerli, bsc, bscTestnet];
+const projectId = 'f85ccd6a72bcb04b8fa287ba34b90bf2';
+
+const { provider } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 2, chains }),
+  provider
+})
+
+const ethereumClient = new EthereumClient(wagmiClient, chains)
+function App() {
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <Homepage />
+        <Web3NetworkSwitch/>
+      </WagmiConfig>
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
+  )
+}
+
+export default App;
+
+/*
+import { Core } from "@walletconnect/core";
+import SignClient from "@walletconnect/sign-client";
+import {AuthClient}  from "@walletconnect/auth-client";
+
+
+const { isOpen, open, close, setDefaultChain } = useWeb3Modal();
+// setDefaultChain("polygon");
+const options = {
+  route: "Account" | "ConnectWallet" | "Help" | "SelectNetwork"
+  };
+  if (isOpen) {
+    open(options).then(() => {
+     
+   })
+  }
+
+// First instantiate a separate `Core` instance.
+const core = new Core({
+  projectId: "f85ccd6a72bcb04b8fa287ba34b90bf2",
+});
+
+const metadata = {
+  name: "Example Dapp",
+  description: "Example Dapp",
+  url: "#",
+  icons: ["https://walletconnect.com/walletconnect-logo.png"],
+};
+// Pass `core` to the SignClient on init.
+const signClient = await SignClient.init({ core, metadata });
+
+// Pass `core` to the AuthClient on init.
+const authClient = await AuthClient.init({ core, metadata });
+*/
