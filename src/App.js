f3244d6e3af824c1040a0e0d26c39e975e18dafa
acca3434 <wnqudgus1234@gmail.com>
2023-03-16 16:15:02 +0900

    "fix(App.js) : 20230312 - make mobile Qrcode"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { Web3Modal,Web3NetworkSwitch,Web3Button} from '@web3modal/react'
 import { configureChains, createClient, WagmiConfig,} from 'wagmi'
 import { arbitrum, mainnet, polygon, bsc, bscTestnet, goerli } from 'wagmi/chains'
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector"
+import { useEffect } from 'react'
+// import Homepage from "./view/Homepage"
 const projectData = {
   projectId: process.env.REACT_APP_PRJECTID,
   projectUri: process.env.REACT_APP_PROJECTURI,
@@ -12,16 +15,26 @@ const projectId = projectData.projectId;
 const { provider } = configureChains(chains, [w3mProvider({projectId})])
 const wagmiClient = createClient({
   autoConnect: true,
-  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  connectors: w3mConnectors({ projectId, version: 2, chains }),
   provider
 })
 const ethereumClient = new EthereumClient(wagmiClient, chains)
-function App() {
+const walletconnect = new WalletConnectConnector({
+    rpc: { 1: "https://rpc.ankr.com/eth" },
+    infuraId: "e9761af980254c2e8fcbff94c21dd59e",
+    pollingInterval: 15000,
+});
+    
+async function App() {
+    await walletconnect.activate(walletconnect, undefined, true).catch((err) => {
+    console.log(err);
+    walletconnect.walletConnectProvider = undefined;
+});
   return (
     <>
       <WagmiConfig client={wagmiClient}>
-        <Web3Button />
-        <Web3NetworkSwitch/>깃허브 페이지스
+        <Web3Button/>
+        <Web3NetworkSwitch/>
       </WagmiConfig>
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     </>
