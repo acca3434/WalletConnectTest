d7b10b55cefe2bebe08635ed0c03690f09772399
acca3434 <wnqudgus1234@gmail.com>
2023-03-24 13:10:35 +0900

    feat(walletButton) : create WalletConnect Button and function & module

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { Web3Modal,Web3NetworkSwitch,Web3Button} from '@web3modal/react'
 import { configureChains, createClient, WagmiConfig,} from 'wagmi'
 import { arbitrum, mainnet, polygon, bsc, bscTestnet, goerli } from 'wagmi/chains'
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector"
-import { useEffect } from 'react'
-// import Homepage from "./view/Homepage"
+import WalletConnectConnectorV1 from './view/WalletConnectConnectorV1'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  display : inline-block;
+  font-size: 20px;
+  height : 40px;
+  width : auto;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+`;
 const projectData = {
   projectId: process.env.REACT_APP_PRJECTID,
   projectUri: process.env.REACT_APP_PROJECTURI,
+  projectInfura:process.env.REACT_APP_INFURAID
 };
 const chains = [arbitrum, mainnet, polygon, goerli, bsc, bscTestnet];
 const projectId = projectData.projectId;
@@ -19,21 +30,15 @@ const wagmiClient = createClient({
   provider
 })
 const ethereumClient = new EthereumClient(wagmiClient, chains)
-const walletconnect = new WalletConnectConnector({
-    rpc: { 1: "https://rpc.ankr.com/eth" },
-    infuraId: "e9761af980254c2e8fcbff94c21dd59e",
-    pollingInterval: 15000,
-});
-    
-async function App() {
-    await walletconnect.activate(walletconnect, undefined, true).catch((err) => {
-    console.log(err);
-    walletconnect.walletConnectProvider = undefined;
-});
+
+function App() {
   return (
     <>
       <WagmiConfig client={wagmiClient}>
-        <Web3Button/>
+        <Web3Button />
+        <Button onClick={WalletConnectConnectorV1}>
+          MobileConnectWallet
+        </Button>
         <Web3NetworkSwitch/>
       </WagmiConfig>
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
@@ -42,70 +47,3 @@ async function App() {
 }
 
 export default App;
-
-
-
-// /*
-// import HomePageTest from './view/Web3ModalTest'
-// import { Core } from "@walletconnect/core";
-// import SignClient from "@walletconnect/sign-client";
-// import {AuthClient}  from "@walletconnect/auth-client";
-
-
-// const { isOpen, open, close, setDefaultChain } = useWeb3Modal();
-// // setDefaultChain("polygon");
-// const options = {
-//   route: "Account" | "ConnectWallet" | "Help" | "SelectNetwork"
-//   };
-//   if (isOpen) {
-//     open(options).then(() => {
-     
-//    })
-//   }
-
-// // First instantiate a separate `Core` instance.
-// const core = new Core({
-//   projectId: "f85ccd6a72bcb04b8fa287ba34b90bf2",
-// });
-
-// const metadata = {
-//   name: "Example Dapp",
-//   description: "Example Dapp",
-//   url: "#",
-//   icons: ["https://walletconnect.com/walletconnect-logo.png"],
-// };
-// // Pass `core` to the SignClient on init.
-// const signClient = await SignClient.init({ core, metadata });
-
-// // Pass `core` to the AuthClient on init.
-// const authClient = await AuthClient.init({ core, metadata });
- //     <HomePageTest/>
-// */
-
-
-
-
-// import React from "react";
-// import SignClient from "@walletconnect/sign-client";
-
-// const App = async () => {
-  
-//   const projectData = {
-//   projectId: process.env.REACT_APP_PRJECTID,
-//   projectUri: process.env.REACT_APP_PROJECTURI,
-// };
-//   const signClient = await SignClient.init({
-//   projectId: projectData.projectId,
-//   // optional parameters
-//   relayUrl: `https://relay.walletconnect.com/?projectId=${projectData.projectId}`,
-//   metadata: {
-//     name: "ExamTest",
-//     description: "ExamTest",
-//     url: "https://acca3434.github.io/WalletConnectTest/",
-//     icons: ["https://walletconnect.com/walletconnect-logo.png"],
-//   },
-// });
-//   return <div>App</div>;
-// };
-
-// export default App;
--- src/view/Homepage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Web3Button } from '@web3modal/react'
-// 홈페이지 속성 추가
-const Homepage = () => {
-  return <Web3Button />
-};
-
-export default Homepage;
